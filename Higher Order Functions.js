0c13edd39bbcc77e87a4c62a8e311bb3a9d8cf36
RobertHJones <roberthuwjones@outlook.com>
2021-12-25 11:07:02 +0000

    more

--- Higher Order Functions.js
+++ Higher Order Functions.js
@@ -1,41 +1,54 @@
 function factorOfThree(num) {
-    return num % 3 === 0;
+  return num % 3 === 0;
 }
-let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
-array.filter(factorOfThree)
+array.filter(factorOfThree);
 
 function multiplyByEleven(num) {
-    return num * 11;
+  return num * 11;
 }
 
-array.map(multiplyByEleven)
+array.map(multiplyByEleven);
 
 function numberCubed(num) {
-    return Math.pow(num, 3)
+  return Math.pow(num, 3);
 }
-array.map(numberCubed)
+array.map(numberCubed);
 
-let people = ['Rob', 'Bob', 'Bobby', 'Robbie', 'Robert', 'Bert', 'Bertie', 'Roberto']
+let people = [
+  "Rob",
+  "Bob",
+  "Bobby",
+  "Robbie",
+  "Robert",
+  "Bert",
+  "Bertie",
+  "Roberto",
+];
 
 function addJones(name) {
-    if (name.charAt(0) === 'R') {
-    return `${name} Jonesy`
-} else {
-    return `${name} Who?`
+  if (name.charAt(0) === "R") {
+    return `${name} Jonesy`;
+  } else {
+    return `${name} Who?`;
+  }
 }
-}
 
-people.map(addJones)
+people.map(addJones);
 
- findTotal = (total, num) =>
-    total + num;
+findTotal = (total, num) => total + num;
 
-array.reduce(findTotal)
+array.reduce(findTotal);
 
 function isGreaterThanTwelve(num) {
-return num > 12
+  return num > 12;
 }
 
-array.findIndex(isGreaterThanTwelve)
-array.find(isGreaterThanTwelve)
+array.findIndex(isGreaterThanTwelve);
+array.find(isGreaterThanTwelve);
+
+function lessThan(num) {
+  return (base) => base < num;
+}
+let lessThan50 = lessThan(50);
